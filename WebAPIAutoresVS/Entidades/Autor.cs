4bf516c900ce9a8d03e5f6a0f0f8402ada2de8f1
Alberto Chirinos <developernet@outlook.com>
2024-04-10 10:24:26 -0500

    Default validations are added to the Author class.

--- WebAPIAutoresVS/Entidades/Autor.cs
+++ WebAPIAutoresVS/Entidades/Autor.cs
@@ -1,9 +1,20 @@
-﻿namespace WebAPIAutoresVS.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIAutoresVS.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe tener más de {1} caracteres")]
         public string Nombre { get; set; }
+        [Range(18, 120)]
+        [NotMapped]
+        public int Edad { get; set; }
+        [Url]
+        [NotMapped]
+        public string URL { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
