0b396f4c1f41d203afd5d9206d292436752a416f
Alberto Chirinos <developernet@outlook.com>
2024-06-06 22:42:26 -0500

    filed publication date for books was added

--- WebAPIAutoresVS/DTOs/LibroCreacionDTO.cs
+++ WebAPIAutoresVS/DTOs/LibroCreacionDTO.cs
@@ -5,9 +5,11 @@ namespace WebAPIAutoresVS.DTOs
 {
     public class LibroCreacionDTO
     {
+        [Required]
         [PrimeraLetraMayuscula]
         [StringLength(maximumLength: 250)]
         public string Titulo { get; set; }
+        public DateTime FechaPublicacion { get; set; }
         public List<int> AutoresIds { get; set; }
     }
 }
